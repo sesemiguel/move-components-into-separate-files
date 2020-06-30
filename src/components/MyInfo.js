89eb7eec00529f2362ed7d4cd448c8de11572d7e
Sese <sese.miguel9@gmail.com>
2020-06-30 20:59:50 +0800

    initial commit

---
+++ src/components/MyInfo.js
@@ -0,0 +1,16 @@
+import React from 'react';
+function MyInfo() {
+    return (
+        <div>
+            <h1 style={{ color: "red" }}>Miguel Karlo Sese</h1>
+            <p>I am learning React JS cause I want to.</p>
+            <ul>
+                <li>Coron</li>
+                <li>Dumaguete</li>
+                <li>Siargao</li>
+            </ul>
+        </div>
+    )
+}
+
+export default MyInfo
